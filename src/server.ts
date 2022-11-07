e5601e1874af122dd48cd7e05aad2a1bbcaaa54c
Luis Portugal <luisenriquegomesportugal@gmail.com>
2022-11-07 02:30:54 -0300

    change to deploy on heruko

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import "reflect-metadata"
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import { ApolloServer } from "apollo-server"
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from "@apollo/server/standalone"
 import { buildSchema } from "type-graphql"
 import { resolvers } from "./resolvers"
 
 async function bootstrap() {
     const schema = await buildSchema({ resolvers })
-
     const server = new ApolloServer({ schema })
-    const { url } = await server.listen({port: process.env.PORT || 4000})
+
+    const port = Number.parseInt(process.env.PORT || '4000')
+    const { url } = await startStandaloneServer(server, { listen: {port}})
 
     console.log(`Running on ${url}`)
 }

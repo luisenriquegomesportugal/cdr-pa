5aa8d2e4c9ad8796c8251b85a7cdca8eba998f36
Luis Portugal <luisenriquegomesportugal@gmail.com>
2022-11-07 02:55:48 -0300

    back

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,15 @@ import "reflect-metadata"
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
+import { ApolloServer } from "apollo-server"
 import { buildSchema } from "type-graphql"
 import { resolvers } from "./resolvers"
 
 async function bootstrap() {
     const schema = await buildSchema({ resolvers })
-    const server = new ApolloServer({ schema })
 
-    const port = Number.parseInt(process.env.PORT || '4000')
-    const { url } = await startStandaloneServer(server, { listen: {port}})
+    const server = new ApolloServer({ schema })
+    const { url } = await server.listen({port: process.env.PORT || 4000})
 
     console.log(`Running on ${url}`)
 }
